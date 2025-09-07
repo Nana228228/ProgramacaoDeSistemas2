ac8a566c6f08d6dca262074003403d04c2aaa50b
Naoomi Suguimoto Arakaki <160281458+Nana228228@users.noreply.github.com>
2025-09-07 17:14:07 +0000

    tdPronto

--- lab04/src/App.java
+++ lab04/src/App.java
@@ -3,12 +3,15 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        read();
+        //read();
+        //create();
+        //update();
+        delete();
     }
 
     // READ
     public static void read() throws SQLException {
-        String url = "jdbc:postgresql://aws-1-sa-east-1.pooler.supabase.com:6543/postgres?user=postgres.ohqxmmjnuwgitrckverx&password=HQ9J8#@2025";
+        String url = "jdbc:postgresql://aws-1-sa-east-1.pooler.supabase.com:6543/postgres?user=postgres.beypcfcejddbytjgauap&password=PrimeiroCRUD";
                      
         Connection c = DriverManager.getConnection(url);
         String sql = "SELECT * FROM contas";
@@ -21,6 +24,76 @@ public class App {
         }
         c.close ();
     }
+    // CREATE
+    public static void create() throws SQLException {
+
+    //RECEBENDO ENTRADAS
+    System.out.println("Número para a nova conta: ");
+    long nro = Long.parseLong(System.console().readLine());
+
+    System.out.println("Saldo para a nova conta: ");
+    BigDecimal saldo = new BigDecimal(System.console().readLine());
+
+    //DADOS PARA A CONEXÃO
+    String url = "jdbc:postgresql://aws-1-sa-east-1.pooler.supabase.com:6543/postgres?user=postgres.beypcfcejddbytjgauap&password=PrimeiroCRUD";
+    Connection c = DriverManager.getConnection(url);
+
+    //COMANDO SQL 
+    String sql = "INSERT INTO contas VALUES (?, ?)";
+
+    //USANDO PreparedStatement PARA REALIZAR A OPERAÇÃO NA DB
+    PreparedStatement stm = c.prepareStatement(sql);
+    stm.setLong(1, nro);
+    stm.setBigDecimal(2, saldo);
+    int ret = stm.executeUpdate();
+    System.out.println("Número de registros inseridos: " + ret);
+    c.close();
+    }
+
+    //UPDATE
+    public static void update() throws SQLException {
+        //RECEBENDO ENTRADAS
+        System.out.println("Número de conta já existente: ");
+        long nro = Long.parseLong(System.console().readLine());
+
+        System.out.println("Novo saldo para essa conta: ");
+        BigDecimal saldo = new BigDecimal(System.console().readLine());
+
+        //DADOS PARA A CONEXÃO
+        String url = "jdbc:postgresql://aws-1-sa-east-1.pooler.supabase.com:6543/postgres?user=postgres.beypcfcejddbytjgauap&password=PrimeiroCRUD";
+        Connection c = DriverManager.getConnection(url);
+
+        //COMANDO SQL 
+        String sql = "UPDATE contas SET saldo=? WHERE nro_conta=?";
+
+        //USANDO PreparedStatement PARA REALIZAR A OPERAÇÃO NA DB
+        PreparedStatement stm = c.prepareStatement(sql);     
+        stm.setBigDecimal(1, saldo);
+        stm.setLong(2, nro);
+        int ret = stm.executeUpdate();
+        System.out.println("Número de registros alterados: " + ret);
+        c.close();
+
+    }   
+    
+    public static void delete() throws SQLException{
+        System.out.println("Digite número da conta a ser deletada: ");
+        long nro = Long.parseLong(System.console().readLine());
+
+                //DADOS PARA A CONEXÃO
+        String url = "jdbc:postgresql://aws-1-sa-east-1.pooler.supabase.com:6543/postgres?user=postgres.beypcfcejddbytjgauap&password=PrimeiroCRUD";
+        Connection c = DriverManager.getConnection(url);
+
+        //COMANDO SQL 
+        String sql = "DELETE FROM contas WHERE nro_conta=?";
+        PreparedStatement stm = c.prepareStatement(sql);
+        stm.setLong(1, nro);
+
+        int ret = stm.executeUpdate();
+
+        System.out.println("Número de registros deletados: " + ret);
+    }
+
 }
 
 
